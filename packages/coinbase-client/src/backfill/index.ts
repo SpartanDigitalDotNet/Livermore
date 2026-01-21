44564f8783a5f7f1f56705f7d99191a1f487d5b0
SpartanDigital <1730830+SpartanDigitalDotNet@users.noreply.github.com>
2026-01-21 16:23:50 -0600

    feat(07-01): create backfill module index with re-exports
    - Re-exports StartupBackfillService class
    - Re-exports BackfillConfig type
    - Re-exports DEFAULT_BACKFILL_CONFIG and TIMEFRAME_PRIORITY constants

---
+++ packages/coinbase-client/src/backfill/index.ts
@@ -0,0 +1,3 @@
+export { StartupBackfillService } from './startup-backfill-service';
+export type { BackfillConfig } from './types';
+export { DEFAULT_BACKFILL_CONFIG, TIMEFRAME_PRIORITY } from './types';
