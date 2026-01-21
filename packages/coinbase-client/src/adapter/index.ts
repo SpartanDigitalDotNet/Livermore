e6666a2ac946d0b36066e92bf556ad87e4f379fa
SpartanDigital <1730830+SpartanDigitalDotNet@users.noreply.github.com>
2026-01-21 10:04:12 -0600

    feat(05-01): export CoinbaseAdapter from package barrel
    - Add CoinbaseAdapter to adapter/index.ts barrel export
    - Export CoinbaseAdapterOptions type for consumers

--- packages/coinbase-client/src/adapter/index.ts
+++ packages/coinbase-client/src/adapter/index.ts
@@ -2,3 +2,5 @@
  * Exchange adapter base class and types
  */
 export { BaseExchangeAdapter } from './base-adapter';
+export { CoinbaseAdapter } from './coinbase-adapter';
+export type { CoinbaseAdapterOptions } from './coinbase-adapter';
